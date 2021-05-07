a2960d021a90cff003a411647c7a3f4bfe485038
Thomas Chaigneau <t.chaigneau.tc@gmail.com>
2021-05-07 16:43:41 +0200

    ajout main

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -10,6 +10,19 @@ from dotenv import load_dotenv
 # Chargement des variables d'environnement
 load_dotenv()
 
+class CSV():
+
+    @classmethod
+    def charger_graph(cls):
+        cls.df  = pd.read_csv("coord.csv")
+        cls.df_to_dict = dict([(i,[a,b]) for i, a,b in zip(cls.df.index, cls.df.X, cls.df.Y)])
+        return cls.df_to_dict
+
+    @classmethod
+    def save_graph(cls, value):
+        cls.df = pd.DataFrame.from_dict(data=value, orient='index', columns=['X', 'Y'])
+        cls.df.to_csv('coord.csv', index=False)
+
 class Lieu():
     
     @classmethod
@@ -30,9 +43,9 @@ class Route:
             noeud = file_[0]
             del file_[0]
             marque.append(noeud)
-            point = cls.batiste(noeud)
-            if point not in marque:
-                file_.append(point)
+            points = random.randint(0, os.gentenv("NB_LIEUX"))
+            if points not in marque:
+                file_.append(points)
 
         way = marque.copy()
         way.append(0)
@@ -45,14 +58,12 @@ class Graph():
         cls.liste_lieux = {}
 
         for i in range(NB_LIEUX):
-            cls.x = randint(20, LARGEUR)
-            cls.y = randint(20, HAUTEUR)
+            cls.x = random.randint(20, LARGEUR-20)
+            cls.y = random.randint(20, HAUTEUR-20)
             cls.liste_lieux[i]=Lieu.init_lieu(cls.x, cls.y)
 
         return cls.liste_lieux
-
-
-
+        
     @classmethod
     def calcul_matrice_cout_od(cls, NB_LIEUX, LARGEUR, HAUTEUR):
         cls.coord = cls.creation_points(NB_LIEUX, LARGEUR, HAUTEUR)
@@ -65,7 +76,6 @@ class Graph():
                     cls.result = Lieu.calc_distance(cls.vecteur1, cls.vecteur2)
                     cls.matrix[i,j] = cls.result
                     cls.matrix[j,i] = cls.result
-        print(pd.DataFrame(cls.matrix))
         return cls.matrix
 
     @classmethod
@@ -75,17 +85,29 @@ class Graph():
 
         return cls.passage
 
+# Fenêtre de l'app
+root = tk.Tk()
+root.title("Challenge Spatial - Groupe 2")
+root.geometry("1000x1000")
+root.bind("<Escape>", lambda x: root.destroy())
 
+# Ajout du canvas
+c = tk.Canvas(root, scrollregion=(0,0,500,500), height=os.getenv('HAUTEUR'), width=os.getenv('LARGEUR'))  
+c.pack()
 
-# # Fenêtre de l'app
-# root = tk.Tk()
-# root.title("Challenge Spatial - Groupe 2")
-# root.geometry("1000x1000")
-# root.bind("<Escape>", lambda x: root.destroy())
+# Génération des lieux et enregistrement en csv
+CSV.save_graph(Graph.creation_points(int(os.getenv("NB_LIEUX")), int(os.getenv("LARGEUR")), int(os.getenv("HAUTEUR"))))
+# Affichage des points
+for k, v in CSV.charger_graph().items():
+    if k == 0:
+        c.create_oval(v[0]-12, v[1]-12 ,v[0]+12, v[1]+12, fill="red")
+    else:
+        c.create_oval(v[0]-12, v[1]-12 ,v[0]+12, v[1]+12)
+    c.create_text(v[0], v[1], text=str(k))
 
-# # Ajout du canvas
-# c = tk.Canvas(root, scrollregion=(0,0,500,500), height=os.getenv('HAUTEUR'), width=os.getenv('LARGEUR'))  
-# c.pack()
+# Génération des chemins
+Graph.calcul_matrice_cout_od(int(os.getenv("NB_LIEUX")), int(os.getenv("LARGEUR")), int(os.getenv("HAUTEUR")))
+Route.calcul_distance_route()
 
-# # Lancement de la page
-# root.mainloop()
+# Lancement de la page
+root.mainloop()
